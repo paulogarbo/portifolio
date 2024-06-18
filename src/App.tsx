f9192dc5d6f1ec498ec4ead33b5a9bd7f5a35540
Paulo <paulexgarbo@gmail.com>
2024-06-18 17:39:32 -0300

    config dark mode on tailwindCss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <div>
-      <h1 className="text-9xl">Hello World!</h1>
+      <h1 className="bg-light-50 dark:bg-dark-50  text-9xl text-light-700 dark:text-dark-700">Hello World!</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {
-      
+      colors: {
+        light: {
+          Default: "#ffffff",
+          50: "#f9fafb",
+          100: "#f3f4f6",
+          200: "#e5e7eb",
+          300: "#d1d5db",
+          400: "#9ca3af",
+          500: "#6b7280",
+          600: "#4b5563",
+          700: "#374151",
+          800: "#1f2937",
+          900: "#111827",
+          950: "#030712",
+        },
+        dark: {
+          Default: "#030712",
+          50: "#111827",
+          100: "#1f2937",
+          200: "#374151",
+          300: "#4b5563",
+          400: "#6b7280",
+          500: "#9ca3af",
+          600: "#9E9E9E",
+          700: "#e5e7eb",
+          800: "#f3f4f6",
+          900: "#f9fafb",
+          950: "#ffffff",
+        },  
+      },
     },
   },
   plugins: [],
